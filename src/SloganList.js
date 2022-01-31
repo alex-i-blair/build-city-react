4ffc34028d549d4789aed6da33d3b75508b26b56
Alex Blair <bclan11@gmail.com>
2022-01-31 15:49:42 -0800

    done and done

--- src/SloganList.js
+++ src/SloganList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Slogan from './Slogan';
 
 export default function SloganList({ slogans }) {
   return (
     <section>
       <h2>Your slogans:</h2>
       {/* This kind of component should be familiar by now. It takes in (as props) an array of slogans */}
-      {slogans.map((slogan, i) => <li key={`${slogan}-${i}`}>
-        {slogan}
-      </li>)
+      {slogans.map((slogan, i) => 
+        <Slogan
+          key={slogan + i}
+          slogan={ slogan }
+        />)
       }
       {/* Then map over those slogans and for each slogan, render a Slogan component. Supply a key and a slogan as props to each Slogan component */}
     </section>
