bffd6d893e6d9f729331b1f9326fd024e6c4ed77
Alex Blair <bclan11@gmail.com>
2022-01-31 15:51:49 -0800

    test-pass

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/slogan/i);
+  const linkElement = screen.getByText(/Skyline/i);
   expect(linkElement).toBeInTheDocument();
 });
